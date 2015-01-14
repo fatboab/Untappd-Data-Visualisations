b79c12190dc18612c463732a66d6ec9c650ce0eb
fatboab <bob@bobarnott.com>
2015-01-14 11:55:19 +0000

    If the response isn't a HTTP 200, then output the error and quit. This lets you know when you've busted through the API rate limit etc.

--- untappd.py
+++ untappd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2013, Bob Arnott
+# Copyright (c) 2013 - 2015, Bob Arnott
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
@@ -47,16 +47,20 @@ while True:
     r = requests.get('http://api.untappd.com/v4/user/checkins/' + untappd_user, params=parameters)
     json = r.json()
 
-    # Update the max_id...
-    max_id = json['response']['pagination']['max_id']
+    if json['meta']['code'] == 200:
+        # Update the max_id...
+        max_id = json['response']['pagination']['max_id']
 
-    # Load the checkins into mongo...
-    checkins.insert(json['response']['checkins']['items'])
+        # Load the checkins into mongo...
+        checkins.insert(json['response']['checkins']['items'])
 
-    # If we didn't get 50 checkins then we're done, so break out...
-    count = json['response']['checkins']['count']
-    print "Inserting %i checkins into mongo..." % count
-    if count < 50:
+        # If we didn't get 50 checkins then we're done, so break out...
+        count = json['response']['checkins']['count']
+        print "Inserting %i checkins into mongo..." % count
+        if count < 50:
+            break
+    else:
+        print json['meta']['error_detail']
         break
 
 print "%s now has %i Untappd checkins in MongoDB..." % (untappd_user, checkins.count())
